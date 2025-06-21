e1609b8a8230445004d12facbca01a37996dd9c7
Curtis Puetz <puetzcurtis@gmail.com>
2025-06-21 10:00:11 +0300

    improve cmake app creator script

--- util_scripts/create_exe_in_cpp_project.py
+++ util_scripts/create_exe_in_cpp_project.py
@@ -6,11 +6,16 @@ cml = "CMakeLists.txt"
 
 # Config
 parent_dir = os.path.join(src_dir, "app_benchmarking")
-subproject_name = "reversed"
+subproject_name_suffix = "dict"  # starts with app
+use_subproject_name_suffix_as_executable_name_prefix = True
+executable_name_prefix = "dict_benchmarking"  # if above is True, this doesn't matter
+if use_subproject_name_suffix_as_executable_name_prefix:
+    executable_name_prefix = subproject_name_suffix
+
 
 if __name__ == "__main__":
     parent_cmake_lists = os.path.join(parent_dir, cml)
-    subproject_dir_name = f"app_{subproject_name}"
+    subproject_dir_name = f"app_{subproject_name_suffix}"
     subproject_dir = os.path.join(parent_dir, subproject_dir_name)
     new_cmake_lists = os.path.join(subproject_dir, cml)
     new_main = os.path.join(subproject_dir, "main.cpp")
@@ -20,8 +25,8 @@ if __name__ == "__main__":
     with open(new_cmake_lists, "w") as file:
         file.writelines(
             [
-                f"add_executable({subproject_name}_test main.cpp)\n",
-                f"target_link_libraries({subproject_name}_test PRIVATE pypp_common)",
+                f"add_executable({executable_name_prefix}_test main.cpp)\n",
+                f"target_link_libraries({executable_name_prefix}_test PRIVATE pypp_common)",
             ]
         )
     # add subproject to parent cmakefile
@@ -33,6 +38,8 @@ if __name__ == "__main__":
             [
                 "#include <pypp_util/main_error_handler.h>\n",
                 "#include <cstdlib>  // Required for EXIT_FAILURE\n",
+                "\n",
+                "\n",
                 "int main() {\n",
                 "    try {\n",
                 "        return 0;\n",
