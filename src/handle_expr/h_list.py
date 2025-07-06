20ef503e7fc98600e46eab22b04269eb4bb7b282
Curtis Puetz <puetzcurtis@gmail.com>
2025-07-06 16:39:53 +0300

    notes

--- src/handle_expr/h_list.py
+++ src/handle_expr/h_list.py
@@ -4,8 +4,8 @@ from src.d_types import QInc
 from src.util.calc_move_args import calc_move_args
 from src.util.ret_imports import RetImports, add_inc
 
-# TODO: I need like a 'list_reserve' function that can be used to reserve space in the
-#  underlying C++ std::vector. This lets Py++ users improve performance.
+# TODO later: I need like a 'list_reserve' function that can be used to reserve space
+#  in the underlying C++ std::vector. This lets Py++ users improve performance.
 
 
 # Note: inline list creation is a little inefficient just because initializer lists in
