de639f1910b74d7c43165a0e6c65d452143cdcf6
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-08 21:06:26 -0700

    fix test directory

--- pypp_core/util_scripts/calling_cli/util.py
+++ pypp_core/util_scripts/calling_cli/util.py
@@ -8,5 +8,5 @@ dirname: str = os.path.dirname(__file__)
 
 def run_cli(args):
     sys.argv = ["prog"] + args  # "prog" simulates the script name
-    test_dir = os.path.join(dirname, "../../../../../pypp_test_dir/main_test_proj")
+    test_dir = os.path.join(dirname, "../../test_dir")
     main_cli(test_dir)
