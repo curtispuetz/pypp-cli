a6d389839774692d896b74bbafe2b3dd41d2e98c
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-09 21:47:41 -0700

    install package

---
+++ pypp_core/util_scripts/calling_cli/install.py
@@ -0,0 +1,9 @@
+from pypp_core.util_scripts.calling_cli.util import run_cli
+
+if __name__ == "__main__":
+    run_cli(
+        [
+            "install",
+            r"C:\Users\puetz\PycharmProjects\pypp-bridge-library-test-0\dist\pypp_bridge_library_test_0-0.0.0-py3-none-any.whl",
+        ]
+    )
