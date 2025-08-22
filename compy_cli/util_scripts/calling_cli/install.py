d471189e826c3e4ee7210fd9a65307891ccb60bf
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-22 15:05:40 -0700

    rename to compy. step 5

--- compy_cli/util_scripts/calling_cli/install.py
+++ compy_cli/util_scripts/calling_cli/install.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     run_cli(
         [
             "install",
-            r"C:\Users\puetz\PycharmProjects\pypp-bridge-library-test-0"
-            r"\dist\pypp_bridge_library_test_0-0.0.0-py3-none-any.whl",
-            r"C:\Users\puetz\PycharmProjects\pypp-bridge-library-test-1"
-            r"\dist\pypp_bridge_library_test_1-0.0.0-py3-none-any.whl",
+            r"C:\Users\puetz\PycharmProjects\compy-bridge-library-test-0"
+            r"\dist\compy_bridge_library_test_0-0.0.0-py3-none-any.whl",
+            r"C:\Users\puetz\PycharmProjects\compy-bridge-library-test-1"
+            r"\dist\compy_bridge_library_test_1-0.0.0-py3-none-any.whl",
         ]
     )
