6965563dcffd14796871d8a5d175bef8d24517d2
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-22 11:41:46 -0700

    remove completed todo

--- pypp_core/src/handle_expr/h_list.py
+++ pypp_core/src/handle_expr/h_list.py
@@ -3,9 +3,6 @@ import ast
 from pypp_core.src.d_types import QInc
 from pypp_core.src.deps import Deps
 
-# TODO later: I need like a 'list_reserve' function that can be used to reserve space
-#  in the underlying C++ std::vector. This lets Py++ users improve performance.
-
 
 # Note: inline list creation is a little inefficient just because initializer lists in
 # C++ are a little inefficient. For small data though, they are fine.
