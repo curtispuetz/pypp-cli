1a688c0c6c524cfffebc75d8d6fbea927ef8fecb
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-24 18:05:48 -0700

    add next glfw example

---
+++ compy_cli/test_dir/python/src/glfw_test_2.py
@@ -0,0 +1,62 @@
+import glfw
+from compy_python.nones import NULL
+from compy_python.strings import to_c_string
+
+
+def key_callback(
+    _window: glfw._GLFWwindow, key: int, _scancode: int, action: int, _mods: int
+):
+    if action == glfw.PRESS:
+        print(f"Key {key} pressed")
+    elif action == glfw.RELEASE:
+        print(f"Key {key} released")
+
+
+def mouse_button_callback(
+    _window: glfw._GLFWwindow, button: int, action: int, _mods: int
+):
+    if action == glfw.PRESS:
+        print(f"Mouse button {button} pressed")
+    elif action == glfw.RELEASE:
+        print(f"Mouse button {button} released")
+
+
+def cursor_position_callback(_window: glfw._GLFWwindow, xpos: float, ypos: float):
+    print(f"Mouse moved to ({xpos}, {ypos})")
+
+
+def glfw_test_2():
+    # Initialize the library
+    if not glfw.init():
+        raise Exception("Failed to initialize GLFW")
+    # Create a windowed mode window and its OpenGL context
+    window: glfw._GLFWwindow = glfw.create_window(
+        640, 480, to_c_string("Hello World"), NULL, NULL
+    )
+    if not window:
+        glfw.terminate()
+        raise Exception("Failed to create GLFW window")
+
+    # Make the window's context current
+    glfw.make_context_current(window)
+
+    # Set input callbacks
+    glfw.set_key_callback(window, key_callback)
+    glfw.set_mouse_button_callback(window, mouse_button_callback)
+    glfw.set_cursor_pos_callback(window, cursor_position_callback)
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Render here, e.g. using pyOpenGL
+
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+        # Poll for and process events
+        glfw.poll_events()
+
+    glfw.terminate()
+
+
+if __name__ == "__main__":
+    glfw_test_2()
