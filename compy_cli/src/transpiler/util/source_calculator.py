290089fc798cacca03b3793cc9c2fd55d3b50b68
Curtis Puetz <puetzcurtis@gmail.com>
2025-08-28 23:37:11 -0700

    dry

--- compy_cli/src/transpiler/util/source_calculator.py
+++ compy_cli/src/transpiler/util/source_calculator.py
@@ -18,17 +18,7 @@ from compy_cli.src.transpiler.util.ret_imports import RetImports
 def calc_main_cpp_source(
     main_py: ast.Module, maps: Maps, src_py_files: list[Path]
 ) -> str:
-    cpp_inc_map, i, py_imports = handle_import_stmts(main_py.body, maps, src_py_files)
-    d: Deps = Deps(
-        DepsDeps(
-            RetImports(set(), set(), cpp_inc_map),
-            [],
-            py_imports,
-            maps,
-            handle_expr,
-            handle_stmt,
-        )
-    )
+    i, d = _create_deps(main_py, maps, src_py_files)
     d.add_inc(QInc("cstdlib"))
     cpp_source_minus_includes: str = handle_main_stmts(main_py.body[i:], d)
     cpp_includes: str = calc_includes_for_main_file(d.ret_imports)
@@ -38,17 +28,7 @@ def calc_main_cpp_source(
 def calc_src_file_cpp_and_h_source(
     src_py: ast.Module, h_file: Path, maps: Maps, src_py_files: list[Path]
 ) -> tuple[str, str]:
-    cpp_inc_map, i, py_imports = handle_import_stmts(src_py.body, maps, src_py_files)
-    d: Deps = Deps(
-        DepsDeps(
-            RetImports(set(), set(), cpp_inc_map),
-            [],
-            py_imports,
-            maps,
-            handle_expr,
-            handle_stmt,
-        )
-    )
+    i, d = _create_deps(src_py, maps, src_py_files)
     cpp_source_minus_include: str = d.handle_stmts(src_py.body[i:])
     h_includes, cpp_includes = calc_includes(d.ret_imports)
     if cpp_source_minus_include.strip() == "":
@@ -59,3 +39,21 @@ def calc_src_file_cpp_and_h_source(
     ret_h_str: str = "#pragma once\n\n" + h_includes + " ".join(d.ret_h_file)
 
     return ret_cpp_str, ret_h_str
+
+
+def _create_deps(
+    module: ast.Module, maps: Maps, src_py_files: list[Path]
+) -> tuple[int, Deps]:
+    cpp_inc_map, i, py_imports = handle_import_stmts(module.body, maps, src_py_files)
+    d: Deps = Deps(
+        DepsDeps(
+            RetImports(set(), set(), cpp_inc_map),
+            [],
+            py_imports,
+            maps,
+            handle_expr,
+            handle_stmt,
+        )
+    )
+
+    return i, d
