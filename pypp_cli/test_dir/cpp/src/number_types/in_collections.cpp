153ff281720d6ccb7cc940a4bdd66580990ee987
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-05 12:10:03 -0700

    test using specific number types in collections

--- pypp_cli/test_dir/cpp/src/number_types/in_collections.cpp
+++ pypp_cli/test_dir/cpp/src/number_types/in_collections.cpp
@@ -1,7 +1,98 @@
 #include "number_types/in_collections.h"
+#include "py_dict.h"
+#include "py_list.h"
+#include "py_set.h"
 #include "py_str.h"
 #include "pypp_util/print.h"
+#include <cstdint>
+
+void _lists() {
+    PyList<int8_t> a({1, 2, 3, 4});
+    print(a);
+    PyList<int16_t> b({5, 6, 7, 8});
+    print(b);
+    PyList<int32_t> c({9, 10, 11, 12});
+    print(c);
+    PyList<int64_t> d({13, 14, 15, 16});
+    print(d);
+    PyList<uint8_t> e({17, 18, 19, 20});
+    print(e);
+    PyList<uint16_t> f({21, 22, 23, 24});
+    print(f);
+    PyList<uint32_t> g({25, 26, 27, 28});
+    print(g);
+    PyList<uint64_t> h({29, 30, 31, 32});
+    print(h);
+    PyList<float> i({3.14, 2.71, 1.61});
+    print(i);
+}
+
+void _sets() {
+    PySet<int8_t> a({1, 2, 3, 4});
+    print(a);
+    PySet<int16_t> b({5, 6, 7, 8});
+    print(b);
+    PySet<int32_t> c({9, 10, 11, 12});
+    print(c);
+    PySet<int64_t> d({13, 14, 15, 16});
+    print(d);
+    PySet<uint8_t> e({17, 18, 19, 20});
+    print(e);
+    PySet<uint16_t> f({21, 22, 23, 24});
+    print(f);
+    PySet<uint32_t> g({25, 26, 27, 28});
+    print(g);
+    PySet<uint64_t> h({29, 30, 31, 32});
+    print(h);
+    PySet<float> i({3.14, 2.71, 1.61});
+    print(i);
+}
+
+void _dicts() {
+    PyDict<int8_t, PyStr> a = {{1, PyStr("a")}, {2, PyStr("b")}};
+    print(a);
+    PyDict<int16_t, PyStr> b = {{3, PyStr("c")}, {4, PyStr("d")}};
+    print(b);
+    PyDict<int32_t, PyStr> c = {{5, PyStr("e")}, {6, PyStr("f")}};
+    print(c);
+    PyDict<int64_t, PyStr> d = {{7, PyStr("g")}, {8, PyStr("h")}};
+    print(d);
+    PyDict<uint8_t, PyStr> e = {{9, PyStr("i")}, {10, PyStr("j")}};
+    print(e);
+    PyDict<uint16_t, PyStr> f = {{11, PyStr("k")}, {12, PyStr("l")}};
+    print(f);
+    PyDict<uint32_t, PyStr> g = {{13, PyStr("m")}, {14, PyStr("n")}};
+    print(g);
+    PyDict<uint64_t, PyStr> h = {{15, PyStr("o")}, {16, PyStr("p")}};
+    print(h);
+    PyDict<float, PyStr> i = {{3.14, PyStr("q")}, {2.71, PyStr("r")}};
+    print(i);
+    PyDict<int16_t, float> j = {{1, 3.14}, {2, 2.71}};
+    print(j);
+    PyDict<uint16_t, int32_t> k = {{1, 1}, {2, 2}};
+    print(k);
+}
+
+PyList<uint16_t> _list_factory() {
+    PyList<uint16_t> ret({1, 2, 3, 4});
+    return ret;
+}
+
+PySet<uint16_t> _set_factory() {
+    PySet<uint16_t> ret({1, 2, 3, 4});
+    return ret;
+}
+
+PyDict<uint16_t, PyStr> _dict_factory() {
+    return {{1, PyStr("a")}, {2, PyStr("b")}};
+}
 
 void number_types_in_collections_fn() {
     print(PyStr("NUMBER TYPES IN COLLECTIONS RESULTS:"));
+    _lists();
+    _sets();
+    _dicts();
+    print(_list_factory());
+    print(_set_factory());
+    print(_dict_factory());
 }
--- pypp_cli/test_dir/python/src/number_types/in_collections.py
+++ pypp_cli/test_dir/python/src/number_types/in_collections.py
@@ -11,7 +11,95 @@ from pypp_python import (
 )
 
 
+def _lists():
+    a: list[int8_t] = [1, 2, 3, 4]
+    print(a)
+    b: list[int16_t] = [5, 6, 7, 8]
+    print(b)
+    c: list[int32_t] = [9, 10, 11, 12]
+    print(c)
+    d: list[int64_t] = [13, 14, 15, 16]
+    print(d)
+    e: list[uint8_t] = [17, 18, 19, 20]
+    print(e)
+    f: list[uint16_t] = [21, 22, 23, 24]
+    print(f)
+    g: list[uint32_t] = [25, 26, 27, 28]
+    print(g)
+    h: list[uint64_t] = [29, 30, 31, 32]
+    print(h)
+    i: list[float32] = [3.14, 2.71, 1.61]
+    print(i)
+
+
+def _sets():
+    a: set[int8_t] = {1, 2, 3, 4}
+    print(a)
+    b: set[int16_t] = {5, 6, 7, 8}
+    print(b)
+    c: set[int32_t] = {9, 10, 11, 12}
+    print(c)
+    d: set[int64_t] = {13, 14, 15, 16}
+    print(d)
+    e: set[uint8_t] = {17, 18, 19, 20}
+    print(e)
+    f: set[uint16_t] = {21, 22, 23, 24}
+    print(f)
+    g: set[uint32_t] = {25, 26, 27, 28}
+    print(g)
+    h: set[uint64_t] = {29, 30, 31, 32}
+    print(h)
+    i: set[float32] = {3.14, 2.71, 1.61}
+    print(i)
+
+
+def _dicts():
+    a: dict[int8_t, str] = {1: "a", 2: "b"}
+    print(a)
+    b: dict[int16_t, str] = {3: "c", 4: "d"}
+    print(b)
+    c: dict[int32_t, str] = {5: "e", 6: "f"}
+    print(c)
+    d: dict[int64_t, str] = {7: "g", 8: "h"}
+    print(d)
+    e: dict[uint8_t, str] = {9: "i", 10: "j"}
+    print(e)
+    f: dict[uint16_t, str] = {11: "k", 12: "l"}
+    print(f)
+    g: dict[uint32_t, str] = {13: "m", 14: "n"}
+    print(g)
+    h: dict[uint64_t, str] = {15: "o", 16: "p"}
+    print(h)
+    i: dict[float32, str] = {3.14: "q", 2.71: "r"}
+    print(i)
+    j: dict[int16_t, float32] = {1: 3.14, 2: 2.71}
+    print(j)
+    k: dict[uint16_t, int32_t] = {1: 1, 2: 2}
+    print(k)
+
+
+def _list_factory() -> list[uint16_t]:
+    # Note: you have to explicitly create the variable and return it.
+    #  You can't just do: return [1, 2, 3, 4]. Because the type can't be inferred
+    #  properly by the C++ compiler.
+    ret: list[uint16_t] = [1, 2, 3, 4]
+    return ret
+
+
+def _set_factory() -> set[uint16_t]:
+    ret: set[uint16_t] = {1, 2, 3, 4}
+    return ret
+
+
+def _dict_factory() -> dict[uint16_t, str]:
+    return {1: "a", 2: "b"}
+
+
 def number_types_in_collections_fn():
     print("NUMBER TYPES IN COLLECTIONS RESULTS:")
-    # a: list[int8_t] = [1, 2, 3, 4]
-    # print(a)
+    _lists()
+    _sets()
+    _dicts()
+    print(_list_factory())
+    print(_set_factory())
+    print(_dict_factory())
