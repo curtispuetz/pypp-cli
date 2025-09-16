10ef700342b03b9b4737ac67fa5d515a2d35343d
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-16 12:01:51 -0700

    make python file detector more efficient

--- pypp_cli/src/transpilers/library/file_changes/file_loader.py
+++ pypp_cli/src/transpilers/library/file_changes/file_loader.py
@@ -3,8 +3,12 @@ from pathlib import Path
 
 def calc_all_py_files(root: Path) -> list[Path]:
     ret: list[Path] = []
-    for path in root.rglob("*.py"):
-        r = path.relative_to(root)
-        if r.parts[0] != ".venv":
-            ret.append(r)
+    for path in root.iterdir():
+        if path.name == ".venv":
+            continue
+        if path.is_dir():
+            for p in path.rglob("*.py"):
+                ret.append(p.relative_to(root))
+        if path.is_file() and path.suffix == ".py":
+            ret.append(path.relative_to(root))
     return ret
