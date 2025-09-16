1a28165ca70ce7c4923be4b4470685d53445273a
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-16 09:25:35 -0700

    simplify

--- pypp_cli/src/transpilers/library/transpiler/files/src/handle_init_file.py
+++ pypp_cli/src/transpilers/library/transpiler/files/src/handle_init_file.py
@@ -7,9 +7,6 @@ from pypp_cli.src.transpilers.library.transpiler.files.util.calc_includes import
 from pypp_cli.src.transpilers.library.transpiler.d_types import QInc
 
 
-# TODO: make class
-
-
 def calc_h_code_for_init_file(py_ast: ast.Module, file: Path) -> tuple[str, Path]:
     h_file_name = file.parent
     h_file: Path = h_file_name.with_suffix(".h")
@@ -17,27 +14,32 @@ def calc_h_code_for_init_file(py_ast: ast.Module, file: Path) -> tuple[str, Path
     for stmt in py_ast.body:
         if _is_assign_with_name_all(stmt):
             continue
-        if not isinstance(stmt, ast.ImportFrom):
-            raise ValueError(
-                f"Only ImportFrom statements and assigning `__all__` are supported in "
-                f"__init__.py files. "
-                f"Found:\n{ast.dump(stmt, indent=4)}"
-            )
-        if stmt.module is None:
-            raise ValueError(
-                f"Only ImportFrom statements with a module are supported in "
-                f"__init__.py files. Found:\n{ast.dump(stmt, indent=4)}"
-            )
-        if stmt.level != 1:
-            raise ValueError(
-                f"Only ImportFrom statements with a relative import level of 1 "
-                f"are supported in __init__.py files. "
-                f"Found:\n{ast.dump(stmt, indent=4)}"
-            )
+        module_name = _validate(stmt)
         add_include_to_res(
-            QInc(f"{h_file_name.as_posix()}/{stmt.module.replace('.', '/')}.h"), res
+            QInc(f"{h_file_name.as_posix()}/{module_name.replace('.', '/')}.h"), res
         )
     return "".join(res), h_file
+
+
+def _validate(stmt: ast.stmt) -> str:
+    if not isinstance(stmt, ast.ImportFrom):
+        raise ValueError(
+            f"Only ImportFrom statements and assigning `__all__` are supported in "
+            f"__init__.py files. "
+            f"Found:\n{ast.dump(stmt, indent=4)}"
+        )
+    if stmt.module is None:
+        raise ValueError(
+            f"Only ImportFrom statements with a module are supported in "
+            f"__init__.py files. Found:\n{ast.dump(stmt, indent=4)}"
+        )
+    if stmt.level != 1:
+        raise ValueError(
+            f"Only ImportFrom statements with a relative import level of 1 "
+            f"are supported in __init__.py files. "
+            f"Found:\n{ast.dump(stmt, indent=4)}"
+        )
+    return stmt.module
 
 
 def _is_assign_with_name_all(stmt: ast.stmt) -> bool:
