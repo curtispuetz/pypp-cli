396c63651c4b0fe155fb5f5c27ecbb635b0a8df0
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-18 11:50:01 -0700

    fix

--- pypp_cli/util_scripts/calling_cli/run_python.py
+++ pypp_cli/util_scripts/calling_cli/run_python.py
@@ -5,6 +5,6 @@ from pypp_cli.util_scripts.calling_cli.util import calc_test_dir_python_executab
 
 dirname: str = os.path.dirname(__file__)
 if __name__ == "__main__":
-    main_file = os.path.join(dirname, "../../test_dir/python/main.py")
+    main_file = os.path.join(dirname, "../../test_dir/main.py")
     python_executable = calc_test_dir_python_executable()
     subprocess.check_call([python_executable, main_file])
